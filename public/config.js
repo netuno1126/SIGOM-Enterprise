c5559a9864a0e400ac3477861c5321aad235436d
netuno1126 <fabiobarboza.dom@gmail.com>
2026-07-27 20:20:58 -0300

    Atualizar o config.js

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.SIGOM_CONFIG = {
   supabaseUrl: 'https://vstqinwjlhrrouxvwzpx.supabase.co',
   supabasePublishableKey: 'sb_publishable_h6L8h8qo5ENdxpAuRQq4PQ_HVGjpCyH',
-  requireMfa: true,
+  requireMfa: false,
   appName: 'SIGOM — Sistema Integrado de Gestão de Obras Militares'
 };
